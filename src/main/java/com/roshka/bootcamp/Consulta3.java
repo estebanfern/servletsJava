d8a5cb087fb813167b7d261feea16a6f0fe46e15
estebanfern <estebangfernandeza@gmail.com>
2022-12-27 12:56:13 -0300

    Tercera consulta

---
+++ src/main/java/com/roshka/bootcamp/Consulta3.java
@@ -0,0 +1,39 @@
+package com.roshka.bootcamp;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.PrintWriter;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+@WebServlet("/consulta3")
+public class Consulta3 extends Consulta{
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse res){
+        try{
+            Statement stmt = connection.createStatement();
+            res.setContentType("text/html");
+            PrintWriter out = res.getWriter();
+            ResultSet rs = stmt
+                    .executeQuery("SELECT moneda.nombre, count(moneda.nombre) cantidad_de_facturas FROM moneda INNER JOIN factura ON moneda.id=factura.moneda_id " +
+                            "GROUP BY (moneda.nombre) ORDER BY (cantidad_de_facturas) DESC;");
+            out.println("<html>");
+            out.println("<body>");
+            while (rs.next()){
+                String nombre = rs.getString(1);
+                Integer gasto = rs.getInt(2);
+                out.println("<p>NOMBRE = " + nombre + "</p>");
+                out.println("<p>CANTIDAD DE FACTURAS = " + gasto + "</p>");
+                out.println("<br>");
+            }
+            out.println("<html>");
+            out.println("<body>");
+            rs.close();
+            stmt.close();
+        }catch (Exception e){
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+        }
+    }
+}
