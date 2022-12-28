b9c743ef48cf88ad068af2c2e9cf942bec3feee6
estebanfern <estebangfernandeza@gmail.com>
2022-12-28 20:38:22 -0300

    Correccion de importaciones

--- src/main/java/com/roshka/bootcamp/Clientes.java
+++ src/main/java/com/roshka/bootcamp/Clientes.java
@@ -1,11 +1,9 @@
 package com.roshka.bootcamp;
 
-import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
--- src/main/java/com/roshka/bootcamp/Consulta.java
+++ src/main/java/com/roshka/bootcamp/Consulta.java
@@ -4,11 +4,8 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-import java.io.PrintWriter;
 import java.sql.Connection;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 //@WebServlet("/consulta")
 public class Consulta extends HttpServlet{
